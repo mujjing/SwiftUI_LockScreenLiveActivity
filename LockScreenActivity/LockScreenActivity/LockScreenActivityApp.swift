2f0b61b45fa8e5dff83353953ed7877af222a88d
전지훈 <junji636@gmail.com>
2022-11-15 23:38:31 +0900

    first Commit

---
+++ LockScreenActivity/LockScreenActivity/LockScreenActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockScreenActivityApp.swift
+//  LockScreenActivity
+//
+//  Created by 전지훈 on 2022/11/15.
+//
+
+import SwiftUI
+
+@main
+struct LockScreenActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
